60773702aa4bebeeaa7b79768b9dc055eae00845
huan <huan@itcast.com>
2019-05-11 14:58:17 +0800

    修改方法

--- git_test_project/src/huan/FirstClass.java
+++ git_test_project/src/huan/FirstClass.java
@@ -7,5 +7,11 @@ public class FirstClass {
         System.out.println("Hello,test git");
         System.out.println("Hello,test git");
         System.out.println("Hello,test git");
+        //执行test方法
+    }
+    public void test(){
+        int i=1;
+        i=i+3;
+        System.out.println(i);
     }
 }
